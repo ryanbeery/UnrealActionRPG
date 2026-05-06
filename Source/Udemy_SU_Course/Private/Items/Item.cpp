5b7afe49fb2a3272668b502fad9970dbe4f06c4c
Ryan Beery <55211837+ryanbeery@users.noreply.github.com>
2026-05-06 15:07:52 -0500

    Moving Objects With Code - Part 1

--- Source/Udemy_SU_Course/Private/Items/Item.cpp
+++ Source/Udemy_SU_Course/Private/Items/Item.cpp
@@ -13,12 +13,15 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 
+	SetActorLocation(FVector(0.f, 0.f, 200.f));
+	SetActorRotation(FRotator(0.f, 45.f, 0.f));
+
 	FVector Location = GetActorLocation();
 	FVector Forward = GetActorForwardVector();
 	FVector MultForward = Location + Forward * 100.f;
 
-	DRAW_BOX(Location, FVector(100.f, 100.f, 100.f));
-	// DRAW_SPHERE(Location);
+	// DRAW_BOX(Location, FVector(100.f, 100.f, 100.f));
+	DRAW_SPHERE(Location);
 	// DRAW_COLOR_SPHERE(Location, FColor::Green);
 	DRAW_VECTOR(Location, MultForward);
 }
