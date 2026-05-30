1da15bab8397b438243fea490a2518ad28b9974c
Ryan Beery <55211837+ryanbeery@users.noreply.github.com>
2026-05-29 22:26:04 -0500

    Exposing functions to blueprints, template functions, components

--- Source/Udemy_SU_Course/Public/Items/Item.h
+++ Source/Udemy_SU_Course/Public/Items/Item.h
@@ -25,8 +25,23 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
 	float TimeConstant = 5.f;
 
+	UFUNCTION(BlueprintPure)
+	float TransformedSin();
+
+	UFUNCTION(BlueprintPure)
+	float TransformedCos();
+
+	template <typename T>
+	T Avg(T First, T Second);
+
 private:
 	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly,
 		meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
 };
+
+template <typename T>
+T AItem::Avg(T First, T Second)
+{
+	return (First + Second) / 2;
+}
