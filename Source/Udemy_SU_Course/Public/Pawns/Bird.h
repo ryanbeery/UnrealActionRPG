213911637624c9cea5af1b5e97de4fd91c59c7c0
Ryan Beery <55211837+ryanbeery@users.noreply.github.com>
2026-06-01 22:20:37 -0500

    The Pawn Class - Lessons 67-68

---
+++ Source/Udemy_SU_Course/Public/Pawns/Bird.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Pawn.h"
+#include "Bird.generated.h"
+
+UCLASS()
+class UDEMY_SU_COURSE_API ABird : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ABird();
+
+	virtual void Tick(float DeltaTime) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(
+		class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UCapsuleComponent* Capsule;
+};
